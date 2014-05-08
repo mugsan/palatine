0a63a00dd915426039b0183d121e0a086a8bf033
johboq <johboq@gmail.com>
2014-05-08 13:56:39 +0200

    player

---
+++ js/gO_player.js
@@ -0,0 +1,21 @@
+function Player(){
+    
+    var mBody = new Rect(10, 26, 26, 42, "#999");
+    var mHead = new rect(10, 10, 26, 26, "#777");
+
+}
+
+Player.prototype.draw = function(context){
+    
+    
+    mBody.draw(context);
+    mHead.draw(context);
+        
+    
+    
+    
+}
+    
+    
+    
+    
