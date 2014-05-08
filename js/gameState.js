6dc7019f14645dadece19921223aa218f38d3c66
Magnus Björk <mugsan@gmail.com>
2014-05-08 14:01:55 +0200

    gameState.js

--- js/gameState.js
+++ js/gameState.js
@@ -1,11 +1,12 @@
-function GameState(){
+function GameState(arg_context){
  
     //this.level = new Level();
     this.mPlayer = new Player();
+    this.mContext = arg_context;
 }
 
 GameState.prototype.draw = function(){
-    this.mPlayer.draw();
+    this.mPlayer.draw(this.mContext);
 }
 
 
