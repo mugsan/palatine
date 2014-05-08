20d347c6729c4ad69d7fda7ed92618ad84f0aa43
johboq <johboq@gmail.com>
2014-05-08 12:21:54 +0200

    added some *.js

---
+++ js/entity.js
@@ -0,0 +1,13 @@
+// -- entity class -- //
+function Entity(){
+    
+    
+    this.x = 0;
+    this.y = 0;
+    
+    
+}
+
+Entity.prototype.draw = function(){
+    
+}
---
+++ js/gameState.js
@@ -0,0 +1,9 @@
+function GameState(){
+ 
+    //this.level = new Level();
+    
+    this.player = new Player();
+    
+    
+    
+}
