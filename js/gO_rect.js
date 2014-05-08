5d7154514e6a48367b045518ffc46c5721128378
Magnus Björk <mugsan@gmail.com>
2014-05-08 12:19:03 +0200

    added gO_rect.js

---
+++ js/gO_rect.js
@@ -0,0 +1,14 @@
+function Rect(arg_x, arg_y, arg_w, arg_h, arg_color){
+    this.left   = arg_x;
+    this.right  = arg_x + arg_w;
+    this.top    = arg_y;
+    this.bottom = arg_y + arg_h;
+    this.width  = arg_w;
+    this.height = arg_h;
+    this.color  = arg_color;
+}
+
+Rect.prototype.draw = function(context) {
+    context.fillstyle = this.color;
+    context.fillrect(this.left, this.top, this.width, this.height);
+}
