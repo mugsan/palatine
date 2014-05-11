baf276820d5cc531455ff227231bd5b3b9ea752a
Magnus Björk <mugsan@gmail.com>
2014-05-11 22:27:35 +0200

    more states

--- js/gameState.js
+++ js/gameState.js
@@ -3,16 +3,12 @@ var State = {
     GAMEOVER:                   1,
     LOADSTAGE:                  2,
     RUNNING:                    3
-
 };
-
-
-
 
 function GameState(){
  
     this.currentLevel           = 0;
-    this.currentState           = 0;
+    this.currentState           = State.INIT;
     this.mLevel                 = new Level();
     this.mLevel.readBMP('./stage/stage1.bmp'); 
 }
@@ -23,13 +19,21 @@ GameState.prototype.draw = function(){
 
 
 GameState.prototype.update = function() {
-    this.mLevel.update(); 
+    switch (this.currentState) {
+        case State.RUNNING:           this.mLevel.update();
+                                break;
+        default:                this.mLevel.update();
+                                
+            
+    }
 };
 
 
 
-function LevelObject(arg_path, arg_color_background, arg_color_foreground){
-    this.path       = arg_path;
-    this.bgColor    = arg_color_background;
-    this.fgColor    = arg_color_foreground;
+function LevelObject(arg_path, arg_color_background, arg_color_foreground, arg_player_pos_x, arg_player_pos_y){
+    this.path                   = arg_path;
+    this.color.background       = arg_color_background;
+    this.color.foreground       = arg_color_foreground;
+    this.player.x               = arg_player_pos_x;
+    this.player.y               = arg_player_pos_y;
 }
