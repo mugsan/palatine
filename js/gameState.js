a3d7a3a9d858d7f6e935362593e4a2ac31463d49
Magnus Björk <mugsan@gmail.com>
2014-05-11 22:05:05 +0200

    added states to gameState

--- js/gameState.js
+++ js/gameState.js
@@ -1,7 +1,18 @@
+var State = {
+    INIT:                       0, 
+    GAMEOVER:                   1,
+    LOADSTAGE:                  2,
+    RUNNING:                    3
+
+};
+
+
+
+
 function GameState(){
  
     this.currentLevel           = 0;
-    //this.mPlayer                = new Player();//move to object level
+    this.currentState           = 0;
     this.mLevel                 = new Level();
     this.mLevel.readBMP('./stage/stage1.bmp'); 
 }
@@ -14,3 +25,11 @@ GameState.prototype.draw = function(){
 GameState.prototype.update = function() {
     this.mLevel.update(); 
 };
+
+
+
+function LevelObject(arg_path, arg_color_background, arg_color_foreground){
+    this.path       = arg_path;
+    this.bgColor    = arg_color_background;
+    this.fgColor    = arg_color_foreground;
+}
