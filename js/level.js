c94c2eef5489e9308b1df936167f9b55402f4f04
Magnus Björk <mugsan@gmail.com>
2014-05-12 17:59:40 +0200

    added bmp palette

--- js/level.js
+++ js/level.js
@@ -1,15 +1,11 @@
 /*
- *  Black   = 23
- *  White   = 255
- *  Red     = 212
- *  LightBlue = 113
- *  
- *  actualValue     : canvasValue
- *  199             : 212       Wall
- *  100             : 113       ConveyerLeft
- *  60              : 74        ConveyerRight
- *  179             : 192       Player
- *  40              : 25        Goal
+ *  Tiles.
+ *  actualValue     : canvasValue   : tileType
+ *  199             : 212           : Wall
+ *  100             : 113           : ConveyerLeft
+ *  60              : 74            : ConveyerRight
+ *  179             : 192           : Player
+ *  40              : 25            : Goal
  *  100             : 121
  *  221             : 230
  *  156             : 174
@@ -30,6 +26,8 @@ function Level(arg_level_data){
     this.mStage                 = 0;        
     this.colorBackground        = arg_level_data.colorBackground;
     this.colorForeground        = arg_level_data.colorForeground;
+    this.colorConveyerBelt      = arg_level_data.colorConveyerBelt;
+    this.colorGoal              = arg_level_data.colorGoal;
     this.width                  = arg_level_data.levelWidth;
     this.height                 = arg_level_data.levelHeight;
 
@@ -60,11 +58,10 @@ Level.prototype.update = function(){
 Level.prototype.draw = function(){
     gContext.beginPath();
     gContext.fillStyle = "#333";
-    gContext.fillRect(0, 0, 320, 240);
+    gContext.fillRect(0, 0, gCanvas.width, gCanvas.height);
     gContext.closePath();
     for (var i = 0, len = this.mStage.length; i < len; i++) {
-            var currentTile = this.mStage[i];
-            if(currentTile.color != "#333") currentTile.draw(gContext);
+        if(this.mStage[i].isSolid) this.mStage[i].draw(gContext);
     }
     this.mPlayer.draw();
 }
@@ -74,7 +71,7 @@ Level.prototype.draw = function(){
 // - Get tile from current stage at arg_X, arg_Y
 Level.prototype.getTile = function(arg_X, arg_Y){
     
-    if(arg_X < 0 || arg_X  > 320 || arg_Y > 240 || arg_Y < 0){
+    if(arg_X < 0 || arg_X  > gCanvas.width || arg_Y > 240 || arg_Y < 0){
         r = new Rect(0, 0, "#333");
         r.isSolid = false;
         return r;
@@ -104,19 +101,6 @@ Level.prototype.readBMP             = function(arg_string) {
 
     var tImageData                  = tContext.getImageData(0, 0, tCanvas.width, tCanvas.height);
     var tPixel                      = new Array();
-    console.log( 
-                    'pixel # 1: ' + tImageData.data[0]+
-                    'pixel # 1: ' + tImageData.data[4]+
-                    'pixel # 1: ' + tImageData.data[8]+
-                    'pixel # 1: ' + tImageData.data[12]+
-                    'pixel # 1: ' + tImageData.data[16]+
-                    'pixel # 2: ' + tImageData.data[20]+
-                    'pixel # 3: ' + tImageData.data[24]+
-                    'pixel # 4: ' + tImageData.data[28]+
-                    'pixel # 5: ' + tImageData.data[32]+
-                    'pixel # 6: ' + tImageData.data[36]+
-                    'pixel # 7: ' + tImageData.data[40]+
-                    'pixel # 8: ' + tImageData.data[44]);
     
         for (var row = 0; row < tCanvas.height; row++) {
             for (var col = 0; col < tCanvas.width; col++) {
